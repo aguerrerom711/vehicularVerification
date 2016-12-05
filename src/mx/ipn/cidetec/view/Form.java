c03da9a4c4385204622af57d958311f679620d93
andres <andres@labquantum>
2016-12-05 13:34:31 -0600

    Second commit

---
+++ src/mx/ipn/cidetec/view/Form.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.ipn.cidetec.view;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author andres
+ */
+public class Form {
+    private String vehicle;
+    
+    private JLabel vehicleTypeLabel;
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public JLabel getVehicleTypeLabel() {
+        return vehicleTypeLabel;
+    }
+
+    public void setVehicleTypeLabel(JLabel vehicleTypeLabel) {
+        this.vehicleTypeLabel = vehicleTypeLabel;
+    }
+    
+    
+    
+    
+    
+}
